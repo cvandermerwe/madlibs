5e12347d04eef8901072cb20767531bd66c0be81
angraf <55407991+angraf@users.noreply.github.com>
2019-09-17 15:34:33 -0400

    Update kyle.js

--- js/adverbs/kyle.js
+++ js/adverbs/kyle.js
@@ -2,4 +2,10 @@ adverbs = adverbs.concat([
     'quickly',
     'foolishly',
     'coyly'
-]);
+    'steezily'
+    'Christinely'
+    'Reevily'
+    'womaningly'
+    'Merylly'
+    'Dworinly'
+    ]);
